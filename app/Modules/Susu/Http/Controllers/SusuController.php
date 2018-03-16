28486fa9b4c795022dec0b136e9f54feea699b1d
苏建 <sujian@hoge.cn>
2018-03-16 10:58:15 +0800

    first commit

--- app/Modules/Susu/Http/Controllers/SusuController.php
+++ app/Modules/Susu/Http/Controllers/SusuController.php
@@ -8,67 +8,13 @@ use Illuminate\Routing\Controller;
 
 class SusuController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Response
-     */
     public function index()
     {
-        print_r('你好');
-        die;
-        return view('susu::index');
+        $a = request('name') ?: '苏苏';
+        $b = request('age') ?: 18;
+        $c = request('sex') ?: '你猜';
+
+        return "我叫{$a},我今年{$b}岁了,我的性别是{$c}";
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
-    {
-        return view('susu::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Show the specified resource.
-     * @return Response
-     */
-    public function show()
-    {
-        return view('susu::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @return Response
-     */
-    public function edit()
-    {
-        return view('susu::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Request $request)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @return Response
-     */
-    public function destroy()
-    {
-    }
 }
--- app/Modules/Susu/Http/routes.php
+++ app/Modules/Susu/Http/routes.php
@@ -2,5 +2,5 @@
 
 Route::group(['prefix' => '', 'namespace' => 'App\\Modules\Susu\Http\Controllers'], function()
 {
-    Route::get('/', 'SusuController@index');
+    Route::any('/', 'SusuController@index');
 });
